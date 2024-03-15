b85a20fcc2d19acfd6daf08c868a3b5005ba92b3
emilgutierrez <emilgutierrez@liceovvh.cl>
2024-03-15 10:50:38 -0300

    ok

---
+++ fundamentos/script.js
@@ -0,0 +1,9 @@
+//ejercicios creacion de variable 
+//una variavle dlobal de tipo var para mostrar el alcance global
+var global = "Global"
+//variable local 
+let local = "Local";
+//variable constante
+const costante = "Costante"
+let myDecimal = 10.45
+console.log("variables: Global: " + Global + "- Local: "+ Local +"- Costante:"+Costante)
