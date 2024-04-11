1776553251afb94575a0b0139cd1c7da501017d1
emilgutierrez <emilgutierrez@liceovvh.cl>
2024-04-11 09:15:19 -0400

    ok

---
+++ desima/ejercicio 3/script.js
@@ -0,0 +1,35 @@
+//Ejercicios Creacion de variables
+
+//Una variable global de tipo
+var global = "Global";
+//Variable Local
+let local = "Local";
+// Constante
+const Constante
+
+console.log("Variables: Global: " + global + "- Local"
+            + local + " - Constante: " + Constante);
+let num = 4; // Numero Entero
+let decimal = 3.1416; // Numero decimal
+let string = "texto"; // cadena de texto
+let bool = true; //Booleano
+let array = ["Vac,",18, "Maki",17, "Gota",2]; //Arreglo            
+let objeto = {   //Tipo Objeto
+                Edad:17
+                Nombre: "Maki"             
+            }:
+let indifinida;  //Indefinida
+let nula = null; //Nulo
+function sumar(num1, num2);{ // Funcion con parametros
+    return num1 + num2;
+} ;
+console.log(
+            sumar(10,5)
+            );
+console.log("Variables:\nEntero: " + num +
+"\nDecimal: " + decimal + "\nString: " + string
++ "\nBoleano: " + bool + "\nArreglo: " + array 
++ "\nObjeto: " + JSON.stringify(objeto) + "\nIndefinida" + indefinida
++ "\nNula: " + nula + "\nResultado de Funcion: " + 
+sumar )
+console.log( sumar() );
---
+++ ejercicio 4/script.js
@@ -0,0 +1,12 @@
+fuction login()
+{
+console.log()
+   let = nombreUsuario = prompt("ingresar su nombre");
+   let = contraseña = prompt("ingrese la contraseña");
+
+   let usuario = "usuario123";
+   let contr = "secreto";
+
+   if (nombreUsuario == usuario && contraseña == contr)
+
+}
