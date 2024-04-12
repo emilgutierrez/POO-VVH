d2157185d423c5c1f0c8090f23453b9c6c56f672
emil gutierrez <emilgutierrez@liceovvh.cl>
2024-04-12 12:47:08 -0400

    ok

--- trabajo 5/bucle 2/scripst.js
+++ trabajo 5/bucle 2/scripst.js
@@ -1,15 +1,50 @@
-function sumade1a10() {
-    let contador = 1;
-    let suma = 0;
-    let cadenaSuma = "";
-    while (contador <= 10) {
-      suma += contador;
-      if (contador !== 10) {
-        cadenaSuma += contador + "+";
-      } else {
-        cadenaSuma += contador;
-      }
-      contador++;
-    }
-    document.getElementById("resultado").innerHTML = "<p>La suma del 1 al 10 es:</p> <br><h2>" + cadenaSuma + " = " + suma + "</h2>";
+}//solucion1
+function bucleWhile() {
+  let count = 1;
+  let suma = 0;
+  let array = [];
+  let sumaStr = "";
+
+  while (count < 10) {
+      suma += count;
+      sumaStr += count + "+";
+      array.push(suma);
+      count++;
   }
+
+  suma += count;
+  sumaStr += count + " = " + suma;
+  document.getElementById("resultado").innerHTML = sumaStr;
+}
+
+// solucion 2
+function sumarNumeros() {
+  let suma = 0;
+  let count = 1;
+  let sumaStr = "";
+
+  while (count <= 10) {
+      suma += count;
+      if (count < 10) {
+          sumaStr += count + "+";
+      } else {
+          sumaStr += count /*+ " = " + suma*/;
+      }
+      count++;
+  }
+  document.getElementById("resultado2").innerHTML =  `${sumaStr} = ${suma} `;
+}
+
+
+// Solucion 3
+function sumarHastaDiez() {
+  let suma = 0;
+  let count = 1;
+  let array = [];
+  while (count <= 10) {
+      array.push(count);
+      suma += count;
+      count++;
+  }
+  document.getElementById("resultado3").innerHTML = `${array.join("+")}=  ${suma}`;
+}
