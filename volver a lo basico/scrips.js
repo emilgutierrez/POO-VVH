bafd84014c743e3880cf6ac74a2c12c79d64b589
emil gutierrez <emilgutierrez@liceovvh.cl>
2024-06-06 09:13:13 -0400

    ok

--- volver a lo basico/scrips.js
+++ volver a lo basico/scrips.js
@@ -1,28 +1,11 @@
-// Definir un array con las rutas de las imágenes
-const imagenes = [
-    "Cheems_el_perrito_meme_murió.jpg",
-    "perro.jpg",
-    "bdcb3f389881a33dcfd785716cc61c67.jpg",
-    "descarga (1).jfif"
-];
-
-// Obtener el botón y la imagen principal
-const botonCambiar = document.getElementById('cambiarImagenBtn');
-const imagenPrincipal = document.getElementById('imagenPrincipal');
-
-// Definir un contador para realizar un seguimiento de la imagen actual
-let contadorImagen = 0;
-
-// Agregar un evento de clic al botón
-botonCambiar.addEventListener('click', () => {
-    // Cambiar la imagen principal a la siguiente en el array
-    imagenPrincipal.src = imagenes[contadorImagen];
-    
-    // Incrementar el contador de imagen
-    contadorImagen++;
-
-    // Si el contador excede el número de imágenes, volver al principio del array
-    if (contadorImagen >= imagenes.length) {
-        contadorImagen = 0;
+// JavaScript code
+function cambiarImagen() {
+    var imagen = document.getElementById("imagen");
+    if (imagen.src.match("imagen1.jpg")) {
+        imagen.src = "imagen2.jpg";
+        imagen.alt = "Imagen 2";
+    } else {
+        imagen.src = "imagen1.jpg";
+        imagen.alt = "Imagen 1";
     }
-});
+}
