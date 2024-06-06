a8945497f63150671aab21b57714ba8385dad522
emil gutierrez <emilgutierrez@liceovvh.cl>
2024-06-06 09:03:31 -0400

    ok

--- volver a lo basico/scrips.js
+++ volver a lo basico/scrips.js
@@ -1,11 +1,11 @@
 // JavaScript code
 function cambiarImagen() {
     var imagen = document.getElementById("imagen");
-    if (imagen.src.match("Cheems el perrito meme, murió.jpg")) {
-        imagen.src = "Cheems el perrito meme, murió.jpg";
-        imagen.alt = "Cheems el perrito meme, murió.jpg";
+    if (imagen.src.match("imagen1.jpg")) {
+        imagen.src = "imagen2.jpg";
+        imagen.alt = "Imagen 2";
     } else {
-        imagen.src = "Cheems el perrito meme, murió.jpg";
-        imagen.alt = "Cheems el perrito meme, murió.jpg";
+        imagen.src = "imagen1.jpg";
+        imagen.alt = "Imagen 1";
     }
 }
