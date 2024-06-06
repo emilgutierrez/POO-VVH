960c83a23c609b45c4321627937cd6526f9057e8
emil gutierrez <emilgutierrez@liceovvh.cl>
2024-06-06 09:07:58 -0400

    ok

--- volver a lo basico/scrips.js
+++ volver a lo basico/scrips.js
@@ -1,11 +1,14 @@
-// JavaScript code
-function cambiarImagen() {
-    var imagen = document.getElementById("imagen");
-    if (imagen.src.match("imagen1.jpg")) {
-        imagen.src = "imagen2.jpg";
-        imagen.alt = "Imagen 2";
-    } else {
-        imagen.src = "imagen1.jpg";
-        imagen.alt = "Imagen 1";
-    }
-}
+// Obtener todas las imágenes en la lista
+const imagenes = document.querySelectorAll('ul li img');
+
+// Iterar sobre cada imagen y agregar un evento de clic
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', () => {
+        // Obtener la ruta de la imagen clicada
+        const nuevaImagenSrc = imagen.getAttribute('src');
+
+        // Obtener la imagen principal y cambiar su src
+        const imagenPrincipal = document.getElementById('imagenPrincipal');
+        imagenPrincipal.setAttribute('src', nuevaImagenSrc);
+    });
+});
