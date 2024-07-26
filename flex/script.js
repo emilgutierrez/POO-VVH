5211b8805dfa6923b9f5e7a80b960b16f8f5cd31
emil gutierrez <emilgutierrez@liceovvh.cl>
2024-07-26 12:17:29 -0400

    ok

--- flex/script.js
+++ flex/script.js
@@ -1,9 +1,5 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    const flexItems = document.querySelectorAll('.flex-item');
-
-    flexItems.forEach((item, index) => {
-        item.style.backgroundColor = index % 2 === 0 ? '#e0e0e0' : '#c0c0c0';
-    });
+    console.log('JavaScript está funcionando correctamente.');
 });
